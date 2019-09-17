286030c2afe4bbd4a4cb5ef1b6d0e41bd9a00b32
ccrockett55 <36276296+ccrockett55@users.noreply.github.com>
2019-09-17 13:26:25 -0500

    changes to form

--- contact.php
+++ contact.php
@@ -1,5 +1,23 @@
 <?php
-if($_POST["message"]) {
-    mail("ccrockett55@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
-}
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST['message'];
+    
+    $email_from = 'li.crockett7@gmail.com';
+    $email_subject = 'New Form';
+
+    $email_body = "User Name: $name.\n".
+                  'User Email: $email.\n'.
+                   'User Message: $message.\n';
+    
+    $to = 'ccrockett55@gmail.com';
+
+    $headers = 'From: $email_form \r\n';
+    $headers = 'Reply-To: $email \r\n';
+
+    mail($to, $email_subject, $email_body, $headers);
+    header('Location: index.html');
+
+
 ?>
